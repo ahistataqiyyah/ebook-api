d22ef7710f9c3ef9a34499c9cc975fc4bd569e96
unknown <ahistapradana04@gmail.com>
2021-01-16 22:29:31 +0700

    challange-author

--- routes/api.php
+++ routes/api.php
@@ -18,16 +18,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource("books", "BookController");
+// Route::resource('books','BookController');
+Route::get('/books', 'BookController@index');
+Route::post('/books/create', 'BookController@store');
+Route::post('/books/update/{id}', 'BookController@update');
+Route::delete('/books/delete/{id}', 'BookController@destroy');
 
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'auth'
-
-], function ($router) {
-    Route::post('login', 'AuthController@login');
-    Route::post('register', 'AuthController@register');
-    Route::post('logout', 'AuthController@logout');
-    Route::post('refresh', 'AuthController@refresh');
-    Route::get('user-profile', 'AuthController@userProfile');
-});
+Route::resource('authors','AuthorController');
